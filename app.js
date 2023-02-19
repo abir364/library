88fbf8e8ecb2bf2eec2f36819b84ddc2d5f09cb0
abir364 <abirh364@gmail.com>
2023-02-19 06:20:02 +0600

    added methods to add books to library

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
+const library = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = Boolean(read);
-
-  this.info = () => {
-    console.log(`${title} is written by ${author} has ${pages}+ pages`);
-    // read ? (console.log(`You are reading ${title}`)) : (console.log(`You are reading ${title}`));
-    if (read) {
-      console.log(`You are reading${title}`);
-    } else {
-      console.log(`You are reading ${title}`);
-    }
-  };
 }
+
+Book.prototype.info = function() {
+  console.log(`${this.title} is written by ${this.author} has ${this.pages} pages`);
+  // read ? (console.log(`You are reading ${title}`)) : (console.log(`You are reading ${title}`));
+  if (this.read) {
+    console.log(`You are reading${this.title}`);
+  } else {
+    console.log(`You are not reading ${this.title}`);
+  }
+};
+
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+function createCard(book) {
+  console.log(book);
+  console.log(this);
+}
+
+function printLibrary() {
+  const cards = document.getElementById('container');
+
+  for (let i = 0; i < library.length; i+=1) {
+    console.log(library[i].title);
+    createCard(library[i]);
+  }
+}
+
+
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 310, false);
 theHobbit.info();
+const theLOR = new Book('The Lord of the Rings', 'J. R. R. Tolkien', 610, true);
+
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(theLOR);
+printLibrary();
