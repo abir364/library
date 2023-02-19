2b2eb908c0b8db5b74bf38f8143d067fed17fda8
abir364 <abirh364@gmail.com>
2023-02-20 04:49:54 +0600

    added a form to add new book fixed the cards

--- app.js
+++ app.js
@@ -17,31 +17,67 @@ Book.prototype.info = function() {
   }
 };
 
-function addBookToLibrary(book) {
-  library.push(book);
-}
 
 function createCard(book) {
-  console.log(book);
-  console.log(this);
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const title = document.createElement('h2');
+  title.textContent=book.title;
+  card.appendChild(title);
+
+  const author = document.createElement('h3');
+  author.textContent=`by ${  book.author}`;
+  card.appendChild(author);
+
+  const pages = document.createElement('p');
+  pages.textContent=`page count: ${  book.pages}`;
+  card.appendChild(pages);
+
+  const read = document.createElement('p');
+  read.textContent= book.read ? 'You read it' : 'You did not read it';
+  card.appendChild(read);
+  
+  return card;
+}
+
+function addBookToLibrary(book) {
+  library.push(book);
+  
+  const cards = document.getElementById('card-container');
+  cards.classList.add('cards');
+  const card = createCard(book);
+  card.setAttribute('data-id', book.title);
+  cards.appendChild(card);
 }
 
 function printLibrary() {
-  const cards = document.getElementById('container');
+  const cards = document.getElementById('card-container');
+  cards.classList.add('cards');
 
   for (let i = 0; i < library.length; i+=1) {
-    console.log(library[i].title);
-    createCard(library[i]);
+    // console.log(library[i].title);
+    cards.appendChild(createCard(library[i]));
   }
 }
 
 
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 310, false);
-theHobbit.info();
-const theLOR = new Book('The Lord of the Rings', 'J. R. R. Tolkien', 610, true);
-
+// theHobbit.info();
+const theLOR = new Book('The Lord of the Rings', 'J. R. R. Tolkien', 1178, true);
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(theLOR);
-printLibrary();
+
+// printLibrary();
+
+const form = document.getElementById('form');
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  console.log(form.read.checked);
+  const newBook = new Book(form.title.value, form.author.value, form.page.value, form.read.checked);
+  addBookToLibrary(newBook);
+  // printLibrary();
+  form.reset();
+});
