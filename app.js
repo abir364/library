feff9824ede40bb053abdc22bb876a3e0a1ad692
abir364 <abirh364@gmail.com>
2023-02-20 19:13:35 +0600

    added functional delete button

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ function createCard(book) {
   const card = document.createElement('div');
   card.classList.add('card');
 
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('deleteBtn');
+  deleteBtn.innerHTML=`<span class="material-symbols-outlined">delete</span>`;
+  deleteBtn.setAttribute('data-id', book.title);
+  card.appendChild(deleteBtn);
+
   const title = document.createElement('h2');
   title.textContent=book.title;
   card.appendChild(title);
@@ -37,31 +43,46 @@ function createCard(book) {
   const read = document.createElement('p');
   read.textContent= book.read ? 'You read it' : 'You did not read it';
   card.appendChild(read);
+
+  card.setAttribute('data-id', book.title);
   
   return card;
 }
 
-function addBookToLibrary(book) {
-  library.push(book);
-  
-  const cards = document.getElementById('card-container');
-  cards.classList.add('cards');
-  const card = createCard(book);
-  card.setAttribute('data-id', book.title);
-  cards.appendChild(card);
-}
+
 
 function printLibrary() {
   const cards = document.getElementById('card-container');
   cards.classList.add('cards');
 
+  if (cards.hasChildNodes) {
+    cards.innerHTML = '';
+  }
+
   for (let i = 0; i < library.length; i+=1) {
     // console.log(library[i].title);
     cards.appendChild(createCard(library[i]));
   }
+  const deleteBtns = document.querySelectorAll('.deleteBtn');
+  deleteBtns.forEach((b) => {
+    b.addEventListener('click', () => {
+    const del = b.dataset.id;
+    const curCard = cards.querySelector(`div[data-id="${del}"]`);
+    cards.removeChild(curCard);
+  });
+});
 }
 
+function addBookToLibrary(book) {
+  library.push(book);
+  printLibrary();
 
+  // const cards = document.getElementById('card-container');
+  // cards.classList.add('cards');
+  // const card = createCard(book);
+  // card.setAttribute('data-id', book.title);
+  // cards.appendChild(card);
+}
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 310, false);
 // theHobbit.info();
@@ -76,7 +97,7 @@ const modal = document.querySelector('.modal');
 const form = document.getElementById('form');
 form.addEventListener('submit',(event)=>{
   event.preventDefault();
-  console.log(form.read.checked);
+  // console.log(form.read.checked);
   const newBook = new Book(form.title.value, form.author.value, form.page.value, form.read.checked);
   addBookToLibrary(newBook);
   // printLibrary();
@@ -93,3 +114,11 @@ const closeModal = document.querySelector('.modal-close');
 closeModal.addEventListener('click', () => {
   modal.classList.add('remove-modal');
 });
+
+// const deleteBtns = document.querySelectorAll('.deleteBtn');
+// deleteBtns.forEach((b) => {
+//   console.log(b)
+//   b.addEventListener('click', () => {
+//     console.log(b.dataset.id);
+//   });
+// });
